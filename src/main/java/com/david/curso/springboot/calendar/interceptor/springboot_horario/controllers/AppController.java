7dba8706b46e530235d721a2a057b83d4ce52d93
David MTZD <davidmtzd21512@gmail.com>
2024-10-02 16:14:39 -0600

    se agrego el mensaje del interceptor

--- src/main/java/com/david/curso/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
+++ src/main/java/com/david/curso/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
@@ -10,15 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/app")
 public class AppController {
 
     @GetMapping("/foo")
-    public ResponseEntity<?> foo() {
+    public ResponseEntity<?> foo(HttpServletRequest request) {
         Map<String, Object> data = new HashMap<>();
         data.put("title", "Bienvenidos al sistema de atencion a clientes!");
         data.put("time", new Date().toString());
+        data.put("message", request.getAttribute("message"));
         return ResponseEntity.ok(data);
     }
 
