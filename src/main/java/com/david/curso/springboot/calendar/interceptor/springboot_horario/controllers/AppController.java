8a4adc73b7ab47e1ae2dca21ab782d81e2499f0d
David MTZD <davidmtzd21512@gmail.com>
2024-10-02 15:45:07 -0600

    se creo la el controlador y getmaping foo

--- src/main/java/com/david/curso/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
+++ src/main/java/com/david/curso/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
@@ -1,5 +1,21 @@
 package com.david.curso.springboot.calendar.interceptor.springboot_horario.controllers;
 
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
 public class AppController {
+
+    @GetMapping("/foo")
+    public ResponseEntity<?> foo() {
+        Map<String, Object> data = Collections.singletonMap("title", "Bienvenidos al sistema de atencion a clientes!");
+        return ResponseEntity.ok(data);
+    }
 
 }
